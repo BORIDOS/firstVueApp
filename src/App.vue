70200d093557eac1da8c0ef1c3f7bc3e712e4148
Boripan Dechthongchai <boripan.dtc@gmail.com>
2026-03-18 13:27:41 +0700

    change web page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<template>
+  <div class="app-container">
+    <h1>📝 Task Manager ของฉัน</h1>
+  </div>
+</template>
+
+<script>
+  //พื้นที่สำหรับเขียน JavaScript (ดึง API, จัดการข้อมูล) จะอยู่ตรงนี้
 </script>
 
-<template>
-  <HelloWorld />
-</template>
+<style>
+  /* พื้นที่สำหรับตกแต่งความสวยงาม (CSS) ของหน้านี้ */
+  .app-container {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+  font-family: sans-serif;
+}
+</style>
