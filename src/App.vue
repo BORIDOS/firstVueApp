9658074c3a9e65f5374b5965df035391c756e09a
Boripan Dechthongchai <boripan.dtc@gmail.com>
2026-03-18 16:03:38 +0700

    add tasklist page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="app-container">
     <h1>📝 Task Manager ของฉัน</h1>
+    <TaskList/>
   </div>
 </template>
 
 <script>
+import TaskList from './components/TaskList.vue';
+
   //พื้นที่สำหรับเขียน JavaScript (ดึง API, จัดการข้อมูล) จะอยู่ตรงนี้
+</script>
+
+<script setup>
+  import TaskList from './components/TaskList.vue';
 </script>
 
 <style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="task-list-container">
+        <h2>📌 รายการ Task ทั้งหมด</h2>
+
+        <!-- วนลูปแสดงข้อมูลจากตัวแปร tasks -->
+        <ul>
+            <li v-for="task in tasks" :key="task.id" class="task-item">
+                <span :class="{ completed: task.isCompleted}">
+                    {{ task.title }}
+                </span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+
+    // สร้างข้อมูลจำลอง (Mock Data) ไปก่อน เดี๋ยว Step ถัดไปเราจะดึงจาก API จริง
+    const tasks = ref ([
+        { id: 1, title: 'ออกแบบ Database', isCompleted: true },
+        { id: 2, title: 'สร้าง Vue Component', isCompleted: false },
+    ]);
+</script>
+
+<style scoped>
+.task-list-container {
+    margin-top: 20px;
+    background-color: #f9f9f9;
+    padding: 15px;
+    border-radius: 8px;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+.task-item {
+    padding: 10px;
+    border-bottom: 1px solid #ddd;
+    font-size: 18px;
+}
+/* ถ้าทำเสร็จแล้ว ให้ขีดฆ่าตัวหนังสือ */
+.completed {
+    text-decoration: line-through;
+    color: #888;
+}
+</style>
