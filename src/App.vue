56f3b5ed82fe4611bb94beda480c522f74715f4a
Boripan Dechthongchai <boripan.dtc@gmail.com>
2026-03-21 21:24:15 +0700

    Modify App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
+<script setup>
+  import TaskList from './components/TaskList.vue';
+</script>
+
 <template>
   <div class="app-container">
     <h1>📝 Task Manager ของฉัน</h1>
     <TaskList/>
   </div>
 </template>
-
-<script>
-import TaskList from './components/TaskList.vue';
-
-  //พื้นที่สำหรับเขียน JavaScript (ดึง API, จัดการข้อมูล) จะอยู่ตรงนี้
-</script>
-
-<script setup>
-  import TaskList from './components/TaskList.vue';
-</script>
 
 <style>
   /* พื้นที่สำหรับตกแต่งความสวยงาม (CSS) ของหน้านี้ */
